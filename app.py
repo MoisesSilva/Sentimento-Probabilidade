01ebeb8a174f2efed947e7784dc260a4c0180e6d
unknown <moisesant.silva13@gmail.com>
2020-07-26 14:52:25 -0300

    commit do modelo

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+import joblib as jb
+import json
 
 app = Flask(__name__)
+
+model = jb.load("model-final3.pkl.z")
 
 cors = CORS(app, resource={r'/*' : {"origins" : "*"}})
 
 @app.route("/", methods=['GET'])
 def index():
-    return "<h1>Olá, Heroku!!"
+    print(request.args)
+
+    mensagem = request.args.get("mensagem", default='')
+    res = {"mensagem": mensagem, "p": model.predict_proba([mensagem])[0][1]}
+    return json.dumps(res)
+
+    #return "<h1>Olá, Heroku!!"
 
 def main():
     port = int(os.environ.get("PORT", 5000))
